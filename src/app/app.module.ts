e73e152f363e1202524d2d1011e3589a70958340
Ben Windsor <benjaminwindsor@hotmail.co.uk>
2018-08-26 20:20:47 +0100

    Upgrade reminder display.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ReminderElementComponent } from './reminder-element/reminder-element.component';
 import { ReminderAddComponent } from './reminder-add/reminder-add.component';
-import { MatListModule,
-  MatCardModule
+import {
+  MatListModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule
 } from "@angular/material";
 import { ReminderContainerComponent } from './reminder-container/reminder-container.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReminderElementComponent,
     ReminderAddComponent,
-    ReminderContainerComponent
+    ReminderContainerComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
